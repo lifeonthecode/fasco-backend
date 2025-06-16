10206e4bbc331e15c7f7894f5dc80759ef2698f9
lifeonthecode <mohammodjuwelislam@gmail.com>
2025-06-17 01:43:06 +0600

    frontend url added

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const app = express();
 // middlewares 
 app.use(express.json())
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://fasco-backend-s4xf.onrender.com'], // you can multiple domain. in side array
+    origin: ['http://localhost:5173', 'https://fasco-frontend-cq7u-fjkfa3rny-mohammodjuwelislams-projects.vercel.app'], // you can multiple domain. in side array
     credentials: true
 }))
 app.use(cookieParser());
